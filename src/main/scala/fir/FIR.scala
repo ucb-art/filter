c7ee395373f170ba9de62b616ad625f58e9d3fe9
Stevo Bailey <stevo.bailey@eecs.berkeley.edu>
2017-03-17 01:05:05 -0700

    [stevo]: no trimming on multiplies, no growth on adds

--- src/main/scala/fir/FIR.scala
+++ src/main/scala/fir/FIR.scala
@@ -4,6 +4,7 @@ import cde._
 import chisel3._
 import chisel3.util._
 import dspjunctions.ValidWithSync
+import dsptools._
 import dsptools.numbers._
 import dsptools.numbers.implicits._
 import dspblocks._
@@ -49,19 +50,21 @@ class FIR[T<:Data:Ring]()(implicit val p: Parameters) extends Module with HasFIR
 
 
   // calculate products as in * tap
-  val products = io.taps.reverse.map { tap => in.map { i => 
-    i * tap
-  }}
+  val products = DspContext.withTrimType(dsptools.NoTrim) {
+    io.taps.reverse.map { tap => in.map { i => 
+      i * tap
+  }}}
 
   // rotates a Seq by i terms, wraps around and can be negative for reverse rotation
   // e.g. (1,2,3) rotate by 1 = (2,3,1)
   def rotate(l: Seq[T], i: Int): Seq[T] = if(i >= 0) { l.drop(i%l.size) ++ l.take(i%l.size) } else { l.takeRight(-i%l.size) ++ l.dropRight(-i%l.size) }
 
   // rotate through, handling weird combinations of taps and input/output lanes
+  val max_size =  log2Up(config.numberOfTaps)+1
   val last = products.reduceLeft { (left: Seq[T], right: Seq[T]) =>
     val reg = Reg(left.last.cloneType)
     reg := left.last
-    right.zip(rotate(left.dropRight(1) :+ reg, -1)).map{case(a, b) => a+b}
+    right.zip(rotate(left.dropRight(1) :+ reg, -1)).map{case(a, b) => DspContext.withOverflowType(dsptools.Wrap) {a+b} }
   }
 
   // all pipeline registers tacked onto end, hopefully synthesis tools handle correctly
--- src/test/scala/fir/FIRSpec.scala
+++ src/test/scala/fir/FIRSpec.scala
@@ -1,9 +1,9 @@
 // See LICENSE for license details.
 package fir
 
+import dsptools._
+import dsptools.numbers._
 import dsptools.numbers.implicits._
-import dsptools.Utilities._
-import dsptools.{DspContext, Grow}
 import spire.algebra.{Field, Ring}
 import breeze.math.{Complex}
 import breeze.linalg._
@@ -12,13 +12,12 @@ import breeze.signal.support._
 import breeze.signal.support.CanFilter._
 import chisel3._
 import chisel3.util._
+import diplomacy._
+import chisel3.experimental._
+import dspblocks._
 import chisel3.iotesters._
 import firrtl_interpreter.InterpreterOptions
-import dsptools.numbers.{DspReal, SIntOrder, SIntRing}
-import dsptools.{DspContext, DspTester, Grow}
 import org.scalatest.{FlatSpec, Matchers}
-import dsptools.numbers.implicits._
-import dsptools.numbers.{DspComplex, Real}
 import scala.util.Random
 import scala.math._
 import org.scalatest.Tag
@@ -28,72 +27,89 @@ import junctions._
 import uncore.tilelink._
 import uncore.coherence._
 
-import dsptools._
 
 object LocalTest extends Tag("edu.berkeley.tags.LocalTest")
 
-class FIRWrapperTester[T <: Data](c: FIRWrapper[T])(implicit p: Parameters) extends DspBlockTester(c) {
-  val config = p(FIRKey)(p)
-  val gk = p(GenKey)
-  val test_length = 10
+class FIRTester[T <: Data](dut: FIRBlockModule[T], input: Seq[Complex], coeffs: Seq[Complex], ref_output: Seq[Complex], verbose: Boolean = false)(implicit p: Parameters) extends DspBlockTester(dut) {
+  val config = p(FIRKey(p(DspBlockId)))
+  val gk = p(GenKey(p(DspBlockId)))
+  val fgk = p(FIRGenKey(p(DspBlockId)))
   
-  // define input datasets here
-  val input = Array.fill(test_length)(Array.fill(gk.lanesIn)(Random.nextDouble*2-1))
-  def rawStreamIn = input
-  val filter_coeffs = Array.fill(config.numberOfTaps)(Random.nextDouble*2-1)
-
-  def doublesToBigInt(in: Array[Double]): BigInt = {
-    in.reverse.foldLeft(BigInt(0)) {case (bi, dbl) =>
-      val new_bi = BigInt(java.lang.Double.doubleToLongBits(dbl))
-      (bi << 64) + new_bi
-    }
+  // define input datasets here, pad zeros
+  if (input.size % gk.lanesIn != 0) {
+    println("Warning: padding zeroes to align input data to input lanes")
   }
-  def streamIn = rawStreamIn.map(doublesToBigInt)
+  val ins = (input ++ Seq.fill(input.size % gk.lanesIn)(Complex(0,0))).grouped(gk.lanesIn).map(Seq(_)).toSeq
+  def streamIn = ins.map(packInputStream(_, gk.genIn))
 
-  // use Breeze FIR filter, but trim (it zero pads the input) and decimate output
-  val expected_output = filter(DenseVector(rawStreamIn.flatten), DenseVector(filter_coeffs)).toArray.drop(config.numberOfTaps-2).dropRight(config.numberOfTaps-2).grouped(gk.lanesIn/gk.lanesOut).map(_.head).toArray
+  require(coeffs.size == config.numberOfTaps, "You passed more or fewer coefficients to the tester than the FIR has taps")
 
   pauseStream
-  //println("Addr Map:")
-  //println(testchipip.SCRAddressMap("FIRWrapper").get.map(_.toString).toString)
-  // assumes coefficients are first addresses
-  filter_coeffs.zipWithIndex.foreach { case(x, i) => axiWrite(i*8, doubleToBigIntBits(x)) }
+  coeffs.zipWithIndex.foreach { case(x, i) => axiWriteAs(addrmap(s"Coefficient_$i"), x, fgk.genCoeff) }
   step(10)
   playStream
-  step(test_length)
-  val output = streamOut.map { x => (0 until gk.lanesOut).map { idx => {
-    val y = (x >> (64 * idx)) & 0xFFFFFFFFFFFFFFFFL
-    java.lang.Double.longBitsToDouble(y.toLong)
-  }}}
+  step(ins.size)
+  val output = unpackOutputStream(gk.genOut, gk.lanesOut)
 
-  //println("Input")
-  //println(rawStreamIn.flatten.deep.mkString(","))
-  //println("Coefficients")
-  //println(filter_coeffs.deep.mkString(","))
-  //println("Chisel Output")
-  //println(output.toArray.flatten.deep.mkString(","))
-  //println("Reference Output")
-  //println(expected_output.deep.mkString(","))
-
-  output.flatten.zip(expected_output).zipWithIndex.foreach { case((chisel, ref), index) => 
-    if (chisel != ref) {
-      val epsilon = 1e-12
-      val err = abs(chisel-ref)/abs(ref+epsilon)
-      assert(err < epsilon || ref < epsilon, s"Error: mismatch on output $index of ${err*100}%\n\tReference: $ref\n\tChisel:    $chisel")
-    }
+  if (verbose) {
+    println("Input")
+    println(input.toArray.deep.mkString("\n"))
+    println("Coefficients")
+    println(coeffs.toArray.deep.mkString("\n"))
+    println("Chisel Output")
+    println(output.toArray.deep.mkString("\n"))
+    println("Reference Output")
+    println(ref_output.toArray.deep.mkString("\n"))
   }
+
+  if (ref_output.size > 0) {
+    require(ref_output.size == gk.lanesOut, "Error: reference output wrong size")
+    output.zip(ref_output).foreach { case (a, b) => require(a == b, "Error: Wrong output!") }
+  }
+
 }
 
 class FIRWrapperSpec extends FlatSpec with Matchers {
-  behavior of "FIRWrapper"
+  behavior of "FIR"
   val manager = new TesterOptionsManager {
-    testerOptions = TesterOptions(backendName = "verilator", testerSeed = 7L)
+    testerOptions = TesterOptions(backendName = "firrtl", testerSeed = 7L)
     interpreterOptions = InterpreterOptions(setVerbose = false, writeVCD = true)
   }
 
   it should "work with DspBlockTester" in {
-    implicit val p: Parameters = Parameters.root(new DspConfig().toInstance)
-    val dut = () => new FIRWrapper[DspReal]()
-    chisel3.iotesters.Driver.execute(dut, manager) { c => new FIRWrapperTester(c) } should be (true)
+    implicit val p: Parameters = Parameters.root(FIRConfigBuilder.standalone(
+      id = "fir", 
+      firConfig = FIRConfig(
+        numberOfTaps = 4,
+        processingDelay = 0,
+        lanesIn = 4,
+        lanesOut = 2), 
+      genIn = () => DspComplex(FixedPoint(8.W, 7.BP), FixedPoint(8.W, 7.BP)),
+      genOut = Some(() => DspComplex(FixedPoint(8.W, 10.BP), FixedPoint(8.W, 10.BP))),
+      genCoeff = Some(() => DspComplex(FixedPoint(11.W, 10.BP), FixedPoint(11.W, 10.BP))))
+      .toInstance)
+    val dut = () => LazyModule(new FIRBlock[DspComplex[FixedPoint]]).module
+    val input = Seq.fill(16)(Complex(0.125, 0.0))
+    val coeffs = Seq.fill(4)(Complex(0.125, 0.0))
+    chisel3.iotesters.Driver.execute(dut, manager) { c => new FIRTester(c, input, coeffs, Seq(), true) } should be (true)
+  }
+
+  it should "not have a DC bias from truncating" in {
+    implicit val p: Parameters = Parameters.root(FIRConfigBuilder.standalone(
+      id = "fir", 
+      firConfig = FIRConfig(
+        numberOfTaps = 128,
+        processingDelay = 0,
+        lanesIn = 4,
+        lanesOut = 2), 
+      genIn = () => DspComplex(FixedPoint(8.W, 7.BP), FixedPoint(8.W, 7.BP)),
+      genOut = Some(() => DspComplex(FixedPoint(8.W, 10.BP), FixedPoint(8.W, 10.BP))),
+      genCoeff = Some(() => DspComplex(FixedPoint(11.W, 10.BP), FixedPoint(11.W, 10.BP))))
+      .toInstance)
+    val dut = () => LazyModule(new FIRBlock[DspComplex[FixedPoint]]).module
+    val input = Seq.fill(128*4)(Complex(0.015625, 0.0))
+    val coeffs = Seq.fill(128)(Complex(0.00390625, 0.0))
+    val output = Seq.fill(2)(Complex(0.0078125, 0))
+    chisel3.iotesters.Driver.execute(dut, manager) { c => new FIRTester(c, input, coeffs, output, true) } should be (true)
   }
 }
